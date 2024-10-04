880ffb5b4db783db7a9d181136a0482a34c977f9
avazbek489 <obidjonovavazbek787@gmail.com>
2024-10-04 19:19:14 +0500

    questions end

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // // // Math functions
 // // 1-MASALA JAVOBI
-// let num = Math.trunc(Math.random() * 100) + 1;
-// console.log(num);
+let num = Math.trunc(Math.random() * 100) + 1;
+console.log(num);
 // // 2-MASALA JAVOBI
 // let num = +prompt("Istalgan bir son kiriting:");
 // if (num >= 0) {
@@ -80,12 +80,76 @@
 // // 5-MASALA JAVOBI
 
 // // 6-MASALA JAVOBI
+// let sum = 0;
+// for (let i = 1; i < 50; i += 2) {
+//   sum += i;
+// }
+// alert(sum);
 // // 7-MASALA JAVOBI
+// let res = "";
+
+// for (let i = 1; i <= 5; i++) {
+//   let age = +prompt(`${i}-odamning yoshini kiriting:`);
+//   if (age >= 18) {
+//     res += `${i}-odam voyaga yetgan\n`;
+//   } else {
+//     res += `${i}-odam voyaga yetmagan\n`;
+//   }
+// }
+// alert(res);
 // // 8-MASALA JAVOBI
+// let sum = 0;
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 2 == 0) {
+//     sum++;
+//   }
+// }
+// alert(sum);
 // // 9-MASALA JAVOBI
+// let count = 0;
+
+// for (let i = 1; i <= 10; i++) {
+//   let num = +prompt(`${i}-sonni kiriting:`);
+//   if (num >= 5 && num <= 10) {
+//     count++;
+//   }
+// }
+// alert("5 va 10 orasidagi sonlar soni" + "  " + count);
 // // 10-MASALA JAVOBI
+// for (let i = 1; i <= 10; i++) {
+//   let num = +prompt(`${i}-sonni kiriting`);
+//   if (num >= 0) {
+//     console.log(num);
+//   }
+// }
 // // 11-MASALA JAVOBI
+// let count = 0;
+// for (let i = 1; i <= 15; i++) {
+//   let num = +prompt(`${i}-sonni kiriting:`);
+//   if (num % 2 == 0 || num < 0) {
+//     count++;
+//   }
+// }
+// alert(count);
 // // 12-MASALA JAVOBI
-// // 13-MASALA JAVOBI
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 3 == 0) {
+//     console.log(i);
+//   }
+// }
 // // 14-MASALA JAVOBI
+// for (let i = 1; i <= 10; i++) {
+//   let num = +prompt(`${i}-sonni kiriting`);
+//   if (num >= 50) {
+//     console.log(num);
+//   }
+// }
 // // 15-MASALA JAVOBI
+// for (let i = 1; i <= 10; i++) {
+//   let num = +prompt(`${i}-sonni kiriting`);
+//   if (num == 0) {
+//     alert("Kiritilgan sonlar ichida 0 mavjud");
+//   } else {
+//     alert("Kiritilgan sonlar ichida 0 mavjud emas");
+//   }
+// }

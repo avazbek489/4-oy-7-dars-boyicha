7ab26c77a39ade5e7115c3f07d3b99c8e9b1bf2f
avazbek489 <obidjonovavazbek787@gmail.com>
2024-10-04 16:54:00 +0500

    ternary end

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// // // Math functions
 // // 1-MASALA JAVOBI
 // let num = Math.trunc(Math.random() * 100) + 1;
 // console.log(num);
@@ -28,6 +29,54 @@
 // let up = Math.ceil(num)
 // let down = Math.floor(num)
 // alert("\nyaxlitlangan son" + up + "\nyaxlitlangan son" + down);
+// // 5-MASALA JAVOBI
+// let num = prompt("Radiusni kiriting");
+// let res = 2 * 3.1415 * num;
+// alert(Math.round(res));
+
+// // // Ternary operator
+// // 1-MASALA JAVOBI
+// let num = prompt("Istalgan son kiriting musbat yoki manfiylikka tekshiriladi");
+// num > 0 ? alert("Kiritilgan son musbat") : alert("Kiritilgan son manfiy");
+// // 2-MASALA JAVOBI
+// let num = prompt("Yoshingizni kiriting");
+// num >= 18 ? alert("Ruxsat etilgan") : alert("Ruxsat etilmagan");
+// // 3-MASALA JAVOBI
+// let num = prompt("Istalgan bir son kiriting");
+// num % 2 == 0 ? alert("Kiritilgan son juft") : alert("Kiritilgan son toq");
+
+// // // for sikl operator
+// // 1-MASALA JAVOBI
+// for (var i = 0; i <= 20; i += 2) {
+//   console.log(i);
+// }
+// // 2-MASALA JAVOBI
+// let sum = 0;
+// for (let i = 1; i <= 10; i++) {
+//   let num = +prompt(`${i}-sonni kiriting`);
+//   if (num > 0) {
+//     sum += num;
+//   }
+// }
+// alert(sum);
+// // 3-MASALA JAVOBI
+// for (let i = 1; i <= 100; i++) {
+//   if (i >= 30 && i <= 50) {
+//     console.log(i);
+//   }
+// }
+// // 4-MASALA JAVOBI
+// let count = 0;
+// let neg = 0;
+// for (let i = 1; i <= 15; i++) {
+//   let num = +prompt(`${i}-sonni kiriting:`);
+//   if (num > 0) {
+//     count++;
+//   } else if (num < 0) {
+//     neg++;
+//   }
+// }
+// alert("Musbatlar" + count + "\nManfiylar" + neg);
 // // 5-MASALA JAVOBI
 
 // // 6-MASALA JAVOBI

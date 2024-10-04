27194d6ec276736a84b40fb0aa5e11987e86ddcc
avazbek489 <obidjonovavazbek787@gmail.com>
2024-10-04 15:35:13 +0500

    beginner

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// // 1-MASALA JAVOBI
+// let num = Math.trunc(Math.random() * 100) + 1;
+// console.log(num);
+// // 2-MASALA JAVOBI
+// let num = prompt("Istalgan bir son kiriting:");
+// if (num >= 0) {
+//   let res = Math.sqrt(num).toFixed(2);
+//   alert(res);
+// } else {
+//   alert("Musbat son kiriting");
+// }
+// // 3-MASALA JAVOBI
+    
+// // 4-MASALA JAVOBI
+// // 5-MASALA JAVOBI
+// // 6-MASALA JAVOBI
+// // 7-MASALA JAVOBI
+// // 8-MASALA JAVOBI
+// // 9-MASALA JAVOBI
+// // 10-MASALA JAVOBI
+// // 11-MASALA JAVOBI
+// // 12-MASALA JAVOBI
+// // 13-MASALA JAVOBI
+// // 14-MASALA JAVOBI
+// // 15-MASALA JAVOBI
